a43dc186baae10ed30cc49172044fb06dc0a0179
Marco <marco.faltoni@outlook.it>
2020-04-22 16:01:01 +0200

    Esercizio Completato - con Variante 1 e Variante 2

--- main.js
+++ main.js
@@ -34,7 +34,7 @@ for (var i = min; i <= max; i++) {
 
 // // VARIANTE ALL'ESERCIZIO 1 - decommentare per visualizzarlo
 // // scrivo un programma che stampi in successione numeri da 1 a 100
-// // quindi devo creare due variabili con un numero minimo e un numero massimo
+// // quindi creo quattro variabili; una con un numero minimo, una un numero massimo, una con il nome stringa Fizz e una con il nome stringa Buzz
 // var min = 1;
 // var max = 100;
 // var mult_3 ='Fizz';
@@ -51,6 +51,34 @@ for (var i = min; i <= max; i++) {
 //         console.log(mult_5);
 //     } else {
 //         console.log(i);
+//     }
+//     // durante la stampa intercetto i numeri multipli sia di 5 che di 3 e sostiuisco alla stampa con il numero la parola FizzBuzz; faccio prima questa operazione rispetto alle altre perché altrimenti FuzzBizz verrebbe sostiuita da Fizz e da Buzz
+//     // durante la stampa intercetto i numeri multipli di 3 e sostiuisco alla stampa con il numero la parola Fizz
+//     // durante la stampa intercetto i numeri multipli di 5 e sostiuisco alla stampa con il numero la parola Buzz
+// }
+
+
+
+
+
+
+// // VARIANTE ALL'ESERCIZIO 2 - decommentare per visualizzarlo
+// // scrivo un programma che stampi in successione numeri da 1 a 100
+// // quindi creo quattro variabili; una con un numero minimo, una un numero massimo, una con l'operazione per il multiplo di 3 e una per il multiplo di 5
+// var min = 1;
+// var max = 100;
+// var mult_3 = i % 3 == 0;
+// var mult_5 = i % 5 == 0;
+//
+// // attivo un ciclo for che va da 1 a 100 assegando ad i il valore minimo e "ciclandolo" fino al valore massimo
+// for (var i = min; i <= max; i++) {
+//
+//     if (mult_3 && mult_5) {
+//         console.log('FizzBuzz');
+//     } else if (mult_3) {
+//         console.log('Fizz');
+//     } else if (mult_5)  {
+//         console.log('Buzz');
 //     }
 //     // durante la stampa intercetto i numeri multipli sia di 5 che di 3 e sostiuisco alla stampa con il numero la parola FizzBuzz; faccio prima questa operazione rispetto alle altre perché altrimenti FuzzBizz verrebbe sostiuita da Fizz e da Buzz
 //     // durante la stampa intercetto i numeri multipli di 3 e sostiuisco alla stampa con il numero la parola Fizz
